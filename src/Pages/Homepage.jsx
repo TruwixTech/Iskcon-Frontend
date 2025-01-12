0466858bd7afd0443a04dba9642677a8ca11aed5
Aftab-truwix <aftaba@truwix.com>
2025-01-12 14:23:55 +0530

    hero section completed

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -21,7 +21,7 @@ const Homepage = () => {
       } else {
         clearInterval(interval); // Clear the interval when animation completes
       }
-    }, 10); // Adjust the interval time to control animation speed
+    }, 15); // Adjust the interval time to control animation speed
     return () => clearInterval(interval);
   }, []);
 
@@ -56,65 +56,88 @@ const Homepage = () => {
   const sunStyle = {
     transform: `translateY(${Math.min(-animationProgress * 4, 500)}px) translateX(${Math.min(animationProgress * 3.4, 800)}px) scale(${1 + animationProgress * 0.040})`, // Move up and right diagonally and scale
   };
-  
-  
 
+  const textStyle = {
+    transform: `translateX(${Math.min(animationProgress * 4, 320)}px)`, // Move down slightly
+  };
 
   return (
     <>
-      <div className="w-full relative">
-        <div className="w-full h-[104vh] fixed top-0">
-        
-          <div className="w-full h-[104vh] relative bg-[#fde3b6] overflow-hidden">
-            <div className="w-full h-[70px] z-[100] absolute top-10 px-20">
+      <div className="w-full h-auto relative font-poppins">
+        {/* Hero Section */}
+        <div className="w-full h-screen relative bg-[#fde3b6] overflow-hidden">
+          <div className="w-full h-[70px] z-[100] absolute top-10 px-20">
             <Navbar />
-            </div>
-         
-            <div className="w-full h-full absolute -top-32 " style={grassStyle}>
-              <img src={grass} alt="grass" className="w-full" />
-            </div>
-            <div className="w-full h-full relative -right-48 z-50">
-              <img
-                src={tree}
-                alt="tree"
-                className="w-full max-h-[880px] absolute -top-[8px]"
-                style={treeStyle}
-              />
-              <img
-                src={krishna}
-                alt="krishna"
-                className="absolute top-1/3 left-[45%] "
-                style={krishnaStyle}
-              />
-              <img
-                src={morpankh}
-                alt="morpankh"
-                className="absolute top-[28%] right-[29%]"
-                style={morpankhStyle}
-              />
-            </div>
-            <div
-              className="w-full h-full absolute -bottom-10 "
-              style={mandirStyle}
-            >
-              <img src={mandir} alt="mandir" className="w-full h-full " />
-            </div>
-            <div
-              className="w-full h-full absolute bottom-0 z-50"
-              style={cloudStyle}
-            >
-              <img src={cloud} alt="cloud" className="w-full h-full" />
-            </div>
-            <div
-              className="w-full h-full absolute bottom-0 z-50"
-              style={cloud2Style}
-            >
-              <img src={cloud2} alt="cloud2" className="w-full h-full" />
-            </div>
-            <div className="w-[100px] h-auto absolute bottom-10 left-[40%] z-40" style={sunStyle}>
-               <img src={Sun} alt="" className=""/>
+          </div>
+
+          <div
+            className="w-full h-full absolute top-[35%] z-50 -left-60"
+            style={textStyle}
+          >
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xl">
+                Connect to SHRI SHRI RADHA MADAN MOHAN JI
+              </h3>
+              <h2 className="text-4xl">Welcome to</h2>
+              <h1 className="text-6xl font-[500]">ISCKON Ghaziabad</h1>
+              <button className="w-[180px] rounded-full bg-white text-[#eb852c] py-2 px-4 cursor-pointer">
+                Donate
+              </button>
             </div>
           </div>
+
+          <div className="w-full h-full absolute -top-32 " style={grassStyle}>
+            <img src={grass} alt="grass" className="w-full" />
+          </div>
+          <div className="w-full h-full relative -right-48 z-50">
+            <img
+              src={tree}
+              alt="tree"
+              className="w-full max-h-[880px] absolute -top-[8px]"
+              style={treeStyle}
+            />
+            <img
+              src={krishna}
+              alt="krishna"
+              className="absolute top-1/3 left-[45%] "
+              style={krishnaStyle}
+            />
+            <img
+              src={morpankh}
+              alt="morpankh"
+              className="absolute top-[28%] right-[29%]"
+              style={morpankhStyle}
+            />
+          </div>
+          <div
+            className="w-full h-full absolute -bottom-10 "
+            style={mandirStyle}
+          >
+            <img src={mandir} alt="mandir" className="w-full h-full " />
+          </div>
+          <div
+            className="w-full h-full absolute bottom-0 z-50"
+            style={cloudStyle}
+          >
+            <img src={cloud} alt="cloud" className="w-full h-full" />
+          </div>
+          <div
+            className="w-full h-full absolute bottom-0 z-50"
+            style={cloud2Style}
+          >
+            <img src={cloud2} alt="cloud2" className="w-full h-full" />
+          </div>
+          <div
+            className="w-[100px] h-auto absolute bottom-10 left-[40%] z-40"
+            style={sunStyle}
+          >
+            <img src={Sun} alt="" className="" />
+          </div>
+        </div>
+
+        {/* Next Section */}
+        <div className="bg-[#fde3b6] h-[600px] text-center pt-20">
+          My font is good
         </div>
       </div>
     </>
