795ac51410e913c7028ded2ab2b8e3635a6a823a
Aftab-truwix <aftaba@truwix.com>
2025-01-13 17:50:43 +0530

    donation added completed

--- src/Components/Admin-Dashboard/AdminDashboard.jsx
+++ src/Components/Admin-Dashboard/AdminDashboard.jsx
@@ -8,7 +8,7 @@ function AdminDashboard() {
             <h1 className='text-center text-4xl font-semibold my-3'>Admin-Dashboard</h1>
             <div className='w-full h-auto flex flex-wrap px-5 my-10 md:px-10 xl:px-20 gap-4 md:gap-8 justify-center items-center'>
                 {
-                    [{ label: "Stats", link: "/admin-dashboard/stats" }, { label: "Events", link: "/admin-dashboard/events" }, { label: "Services", link: "/admin-dashboard/services" }, { label: "Donation", link: "/admin-dashboard/donation" }, { label: "Blogs", link: "/admin-dashboard/blogs" }, { label: "Products", link: "/admin-dashboard/products" }].map((item, index) => (
+                    [{ label: "Stats", link: "/admin-dashboard/stats" }, { label: "Events", link: "/admin-dashboard/events" }, { label: "Services", link: "/admin-dashboard/services" }, { label: "Donation", link: "/admin-dashboard/donation" }, { label: "Blogs", link: "/admin-dashboard/blogs" }, { label: "Products", link: "/admin-dashboard/products" },{ label: "Media", link: "/admin-dashboard/media" }].map((item, index) => (
                         <Link to={item.link} className='w-[400px] h-[200px] p-3 lg:p-5 border rounded-xl shadow-md duration-500 ease-in-out hover:shadow-2xl' key={index}>
                             <h1 className='text-xl md:text-2xl font-bold'>{item.label}</h1>
                         </Link>
--- src/Components/Admin-Dashboard/Donation/AdminDonation.jsx
+++ src/Components/Admin-Dashboard/Donation/AdminDonation.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from 'react';
+import CreateDonationForm from './DonationModal.jsx';
 
 function AdminDonation() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className='w-full h-auto flex flex-col'>
-      <h1 className='text-center text-4xl font-semibold my-3'>Admin-Donation-Section</h1>
+    <div className="w-full h-auto flex flex-col">
+      <h1 className="text-center text-4xl font-semibold my-3">Admin-Donation-Section</h1>
+      <div className="text-center">
+        <button
+          onClick={toggleModal}
+          className="px-4 py-2 border-[#ca8a04] border-2 text-[#ca8a04] rounded hover:bg-[#ca8a04] hover:text-white duration-300"
+        >
+          Create Donation
+        </button>
+      </div>
+      <CreateDonationForm isOpen={isModalOpen} onClose={toggleModal} />
     </div>
-  )
+  );
 }
 
-export default AdminDonation
+export default AdminDonation;
